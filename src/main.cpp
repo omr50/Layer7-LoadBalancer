037a588cec730e00275683a2c52925da33053e20
omr50 <omrab4@gmail.com>
2025-05-21 19:31:22 -0400

    creating forking process to create workers

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
-
+#include <thread>
 
 
 int main() {
+	auto n = std::thread::hardware_concurrency();
 
+	for (int i = 1; i < n; ++i) {
+		pid_t pid = fork();
+		if (pid < 0) perror("fork"), exit(1);
+		// child exits
+		if (pid == 0) break;
+	}
+
+	worker_main();
 	return 0;
 }
